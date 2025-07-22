c5e822c20e48aa735a783613f6d564e1fb341a44
michal-mm <michal@Magic.local>
2025-07-22 12:51:36 +0200

    adding loading currencies from H2 DB

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+VALUES (10001, 'USD', 'PLN', 4, '');
+
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+VALUES (20001, 'EUR', 'PLN', 5, '');
+
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment)
+VALUES (30001, 'GBP', 'PLN', 6, '');
