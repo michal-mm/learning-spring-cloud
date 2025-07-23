4a612787ac8323e4de78c70713b1bda520c339bb
michal-mm <michal@Magic.local>
2025-07-23 14:16:33 +0200

    adding circuit breaker configutation to retry, num of retries and the exponential backoff

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currency_exchange_service/CircuitBreakerController.java
@@ -1,13 +1,31 @@
 package com.in28minutes.microservices.currency_exchange_service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.retry.annotation.Retry;
 
 @RestController
 public class CircuitBreakerController {
+	
+	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
 	@GetMapping("/sample-api")
+	@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
 	public String sampleapi() {
-		return "Sample API";
+		logger.info("Sample api call received");
+		
+		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost/some-dummy-url", String.class);
+		
+		return forEntity.getBody();
+	}
+	
+	public String hardcodedResponse(Exception ex) {
+		logger.info("Fallback to hardcoded for sample-api: " + ex.getMessage());
+		return "Fallback response for sample-api";
 	}
 }
